54578fbbc2363961343cd9964d93334981bdb601
Ali Akbar Azizi <8492767+aliakbarazizi@users.noreply.github.com>
2023-06-29 16:50:39 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import momentJalaali from 'moment-jalaali';
 import DatePicker from './components/DatePicker';
 
-momentJalaali.loadPersian({ dialect: 'persian-modern' });
 export { Calendar } from './components/Calendar';
 export default DatePicker;
